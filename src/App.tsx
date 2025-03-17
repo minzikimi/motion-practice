d9c536e21378dcd85f5717740ea682302ea30880
minzikimi <minji.kim@hyperisland.se>
2025-03-17 15:50:39 +0100

    gradient added as scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,49 @@
 // App.tsx
 
 import styled from "styled-components";
-import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #f0f0f0;
+  
 `;
 
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  width: 100px;
+  height: 100px;
+  background-color: white;
 
 `;
+
 
 
 
 function App() {
   const x = useMotionValue(0);
-  
-  useMotionValueEvent(x, "change", (latest) => {
-    console.log(latest);
-  });
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper>
-      <Box
-        style={{ x }} 
-        drag="x"     
-        dragSnapToOrigin
-        
-      />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
+
 
 export default App;
